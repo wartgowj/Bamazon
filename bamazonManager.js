041cb6e426df51946157a635726df42bdf68b4d5
wartgowj <wartgowj@gmail.com>
2018-02-09 20:36:20 -0800

    challenge 2 complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -28,36 +28,118 @@ function viewLowInventory(connection) {
         if (err) { throw err };
         console.log("----------------------------------------------------------------");
         for (var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].item_id + " " + "Product: " + res[i].product_name.toUpperCase() + " " + "Price: $" + res[i].price + " " + "Qty: " + res[i].stock_quantity);
+            if(res[i].stock_quantity < 5){
+            console.log("Item ID: " + res[i].item_id + " " + "Product: " + res[i].product_name.toUpperCase() + " " + "Qty: " + res[i].stock_quantity);
             console.log("----------------------------------------------------------------")
+            }
         }
         managerPrompt();
     })
 }
 
 //adds inventory to a particular item
-function addInventory(connection) {
-    connection.query('select *from products', function (err, res) {
-        if (err) { throw err };
-        console.log("----------------------------------------------------------------");
-        for (var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].item_id + " " + "Product: " + res[i].product_name.toUpperCase() + " " + "Price: $" + res[i].price + " " + "Qty: " + res[i].stock_quantity);
-            console.log("----------------------------------------------------------------")
-        }
-        managerPrompt();
+function addInventory() {
+    connection.query("select * from products", function (err, res) {
+        inquirer
+            .prompt([
+                {
+                    name: "itemChoice",
+                    type: "input",
+                    message: "Please enter the item ID"
+                },
+                {
+                    name: "itemQuantity",
+                    type: "input",
+                    message: "How many would you like to add?(use negative numbers to reduce inventory"
+                }
+            ])
+            .then(function (answer) {
+                var itemChoice = parseInt(answer.itemChoice);
+                var itemQuantity = parseInt(answer.itemQuantity);
+                var quantityAvail = false;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].item_id === itemChoice) {
+                        quantityAvail = res[i].stock_quantity;
+                    }
+                }
+                if (quantityAvail) {
+                    console.log("--------------------------------------------------")
+                    console.log("You have adjusted item number " + itemChoice + "\'s" + " inventory by " + itemQuantity);
+                    console.log("--------------------------------------------------")
+                    newQuantity = (quantityAvail + itemQuantity);
+                    connection.query("UPDATE products SET ? WHERE ?", [
+                        {
+                            stock_quantity: newQuantity
+                        },
+                        {
+                            item_id: itemChoice
+                        },
+                    ])
+                    managerPrompt();
+                } else {
+                    console.log("------------------------------------------------------------")
+                    console.log("Sorry, that item is not recognized")
+                    console.log("------------------------------------------------------------")
+                    inquirer
+                        .prompt([
+                            {
+                                name: "tryAgain",
+                                type: "confirm",
+                                message: "Would you like to try again?"
+                            },
+                        ]).then(function (response) {
+                            if (response.tryAgain) {
+                                addInventory();
+                            } else {
+                                console.log("------------------------------------------------------------")
+                                console.log("Ok, exiting application")
+                                console.log("------------------------------------------------------------")
+                                connection.end();
+                            }
+                        })
+                }
+            })
     })
 }
 
 //add a new item to the database
-function addProduct(connection) {
-    connection.query('select *from products', function (err, res) {
-        if (err) { throw err };
-        console.log("----------------------------------------------------------------");
-        for (var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].item_id + " " + "Product: " + res[i].product_name.toUpperCase() + " " + "Price: $" + res[i].price + " " + "Qty: " + res[i].stock_quantity);
-            console.log("----------------------------------------------------------------")
+function addProduct(connection){
+    inquirer
+    .prompt([
+        {
+            name: "itemName",
+            type: "input",
+            message: "Please enter the item name"
+        },
+        {
+            name: "itemDept",
+            type: "input",
+            message: "Please enter the item's department"
+        },
+        {
+            name: "itemPrice",
+            type: "input",
+            message: "Please enter the item's price"
+        },
+        {
+            name: "itemInventory",
+            type: "input",
+            message: "Please enter the item's current inventory"
         }
-        managerPrompt();
+    ]).then(function(answer){
+        connection.query("INSERT INTO products SET ?", 
+        {
+            product_name: answer.itemName,
+            department_name: answer.itemDept,
+            price: answer.itemPrice,
+            stock_quantity: answer.itemInventory
+        },
+        function(err, res){
+            console.log("------------------------------------------------------------")
+            console.log(answer.itemName + " has been added to product database")
+            console.log("------------------------------------------------------------")
+            managerPrompt();
+        })      
     })
 }
 
@@ -81,7 +163,7 @@ function managerPrompt(){
             {
                 type: "list",
                 message: "Which process would you like to perform?",
-                choices: ["View Products For Sale", "View Low Inventory", "Add To Inventory", "Add New Product", "Exit"],
+                choices: ["View Products For Sale", "View Low Inventory", "Adjust Inventory", "Add New Product", "Exit"],
                 name: "processChoice"
             }
         ])
@@ -90,17 +172,20 @@ function managerPrompt(){
             switch (managerChoice) {
                 case 'View Products For Sale':
                     getAllProducts(connection);
-                   // break;
+                    break;
                 case 'View Low Inventory':
                     viewLowInventory(connection);
                     break;
-                case 'Add To Inventory':
+                case 'Adjust Inventory':
                     addInventory(connection);
                     break;
                 case 'Add New Product':
                     addProduct(connection);
                     break;
                 case 'Exit':
+                    console.log("------------------------------------------------------------")
+                    console.log("Ok, exiting application")
+                    console.log("------------------------------------------------------------")
                     connection.end();
                     break;
                 default:
